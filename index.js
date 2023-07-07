2d6b77e8584aebafb5c34cac94b59421fefa4af4
Anilkumar50833 <115290903+Anilkumar50833@users.noreply.github.com>
2023-07-07 20:29:27 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log('amazon');
+    console.log("amazon");
   } else if (i % 3 === 0) {
-    console.log('google');
+    console.log("google");
   } else if (i % 5 === 0) {
-    console.log('facebook');
+    console.log("facebook");
   } else {
     console.log(i);
   }
